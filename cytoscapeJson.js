5551bfab38bb6cd5fb38cea309f5cbb12db29139
Harsh Mistry <hnmistry@uwaterloo.ca>
2017-10-27 08:51:36 -0400

    Simplified Promise Chains

--- cytoscapeJson.js
+++ cytoscapeJson.js
@@ -3,62 +3,38 @@ const metadataMapperJson = require('./metadataMapperJson');
 const metadataMapperXML = require('./metadataMapperXML');
 const metadataMapperPC2 = require('./metadataMapperPC2');
 
-//Debug code (Ignore)
-const fs = require('fs');
-//http://identifiers.org/reactome/R-HSA-6804754
-//http://identifiers.org/kegg.pathway/hsa00260
-let x = getCytoscapeJson('http://identifiers.org/kegg.pathway/hsa00260').then(data => fs.writeFileSync('testFile', JSON.stringify(data)));//Debug code (Ignore)
-
 //Get pathway name, description, and datasource
 //Requires a valid pathway uri
 function getPathwayLevelMetadata(uri) {
-  let title, dataSource, comments, organism;
 
-  return (Promise.resolve(pcServices.traversePC2(uri, 'Named/displayName'))
-    .then(data => {
-      title = data.traverseEntry[0].value;
-      return pcServices.traversePC2(uri, 'Entity/dataSource/displayName');
-    })
-    .then(data => {
-      dataSource = data.traverseEntry[0].value;
-      return pcServices.traversePC2(uri, 'Entity/comment');
-    })
-    .then(data => {
-      comments = data.traverseEntry[0].value;
-      return pcServices.traversePC2(uri, 'Entity/organism/displayName');
-    })
-    .then(data => {
-      organism = data.traverseEntry[0].value;
-      return {
-        comments,
-        dataSource,
-        title,
-        organism
-      }
-    }));
-}
+  let title, dataSource, comments, organism;
+  let getValue = data => data.traverseEntry[0].value;
+  let get = path => pcServices.traversePC2(uri, path).then(getValue);
+
+  return Promise.all([
+    get('Named/displayName').then(value => title = value),
+    get('Entity/dataSource/displayName').then(value => dataSource = value),
+    get('Entity/comment').then(value => dataSource = value),
+    get('Entity/organism/displayName').then(value => organism = value)
+  ]).then(data => ({ comments, dataSource, title, organism }));
+};
 
 //Get metadata enhanced cytoscape JSON
 //Requires a valid pathway uri
 function getMetadataJson(uri, parseType) {
   let sbgn, biopax;
+  let downloadType = (parseType === 'pc2' ? 'jsonld' : parseType);
 
-  //Get SBGN XML
-  return pcServices.getPC2(uri, 'sbgn').then(function (data) {
-    sbgn = data;
-
-    let downloadType = (parseType === 'pc2' ? 'jsonld' : parseType);
-
-    //Get BioPax XML
-    return pcServices.getPC2(uri, downloadType).then(function (data) {
-      biopax = data;
-
-      //Map metadata
-      if (parseType === 'jsonld') { return metadataMapperJson(biopax, sbgn); }
-      else if (parseType === 'biopax') { return metadataMapperXML(biopax, sbgn); }
-      else if (parseType === 'pc2') { return metadataMapperPC2(biopax, sbgn); }
-    })
-  });
+  //Get SBGN and Biopax Files
+  return Promise.all([
+    pcServices.getPC2(uri, 'sbgn').then(file => sbgn = file),
+    pcServices.getPC2(uri, downloadType).then(file => biopax = file)
+  ]).then(files => {
+    //Map metadata
+    if (parseType === 'jsonld') { return metadataMapperJson(biopax, sbgn); }
+    else if (parseType === 'biopax') { return metadataMapperXML(biopax, sbgn); }
+    else if (parseType === 'pc2') { return metadataMapperPC2(biopax, sbgn); }
+  })
 }
 
 //Return enhanced cytoscape json
@@ -79,7 +55,6 @@ function getCytoscapeJson(uri, parseType = 'jsonld') {
     return { error: e };
   })
 }
-
 
 module.exports = {
   getCytoscapeJson,
